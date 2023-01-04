ce7113ee0b893b41e092d551a4860b7d26494f33
Abhikrishna S <117815038+s-abhikrishna@users.noreply.github.com>
2023-01-04 19:41:52 +0530

    Create bubblesort.c

---
+++ bubblesort.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+void main()
+{
+	int a[55],temp,n,i,j;
+	printf("enter the length of the array:");
+	scanf("%d",&n);
+	printf("before sorting array:");
+	for(i=0;i<n;i++)
+	{
+	scanf("%d",&a[i]);
+	}
+
+	for(i=0;i<n-1;i++)
+	{
+	for(j=0;j<n-1;j++)
+	{
+	if(a[j]>a[j+1])
+	{
+	temp=a[j];
+	a[j]=a[j+1];
+	a[j+1]=temp;
+	}
+	}
+	}
+	printf("\n listof sorted in Ascending order:");
+	for(i=0;i<n;i++)
+	printf("%d\t",a[i]);
+	printf("\n");
+
+	}
+output
+Enter the range 5
+  3
+  6
+  5
+  2
+  4
+  sort array is 23456
